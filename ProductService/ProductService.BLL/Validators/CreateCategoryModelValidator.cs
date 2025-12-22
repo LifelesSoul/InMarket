f5af524a5566ebf5131e8fc566f36d9abba10d2d
LifelesSoul <lcvrsxlqlxd@gmail.com>
2025-12-22 18:16:16 +0300

    fix

--- ProductService/ProductService.BLL/Validators/CreateCategoryModelValidator.cs
+++ ProductService/ProductService.BLL/Validators/CreateCategoryModelValidator.cs
@@ -8,8 +8,8 @@ public class CreateCategoryModelValidator : AbstractValidator<CreateCategoryMode
     public CreateCategoryModelValidator()
     {
         RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Category name is required.")
-            .MinimumLength(2).WithMessage("Category name must be at least 2 characters long.")
-            .MaximumLength(50).WithMessage("Category name must not exceed 50 characters.");
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .MinimumLength(2).WithMessage("Category name must be at least {MinLength} characters long.")
+            .MaximumLength(50).WithMessage("Category name must not exceed {MaxLength} characters.");
     }
 }
--- ProductService/ProductService.BLL/Validators/UpdateCategoryModelValidator.cs
+++ ProductService/ProductService.BLL/Validators/UpdateCategoryModelValidator.cs
@@ -12,8 +12,8 @@ public class UpdateCategoryModelValidator : AbstractValidator<CategoryModel>
             .NotEmpty();
 
         RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Category name is required.")
-            .MinimumLength(2).WithMessage("Category name must be at least 2 characters long.")
-            .MaximumLength(50).WithMessage("Category name must not exceed 50 characters.");
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .MinimumLength(2).WithMessage("Category name must be at least {MinLength} characters long.")
+            .MaximumLength(50).WithMessage("Category name must not exceed {MaxLength} characters.");
     }
 }
