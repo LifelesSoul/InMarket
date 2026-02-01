5646cffa1e834c84b50a0164c13525c06331543f
Raman Hrynevich <lcvrsxlqlxd@gmail.com>
2026-02-01 19:45:09 +0300

    fix sonar

--- ProductService/ProductService.DAL/Repositories/RabbitMqProducer.cs
+++ ProductService/ProductService.DAL/Repositories/RabbitMqProducer.cs
@@ -1,10 +1,12 @@
 ﻿using ProductService.DAL.Interfaces;
 using RabbitMQ.Client;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Text.Json;
 
 namespace ProductService.DAL.Repositories;
 
+[ExcludeFromCodeCoverage]
 public class RabbitMqProducer : IMessageProducer, IDisposable
 {
     private readonly IModel _channel;
