d546ca06f4988a690b182bfdc4ffb2d32af21a1b
Raman Hrynevich <lcvrsxlqlxd@gmail.com>
2026-02-25 14:57:42 +0300

    add auth to Front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import { ProductList } from './features/Product/components/ProductList';
+import { ProfileHeader } from './features/Auth/components/ProfileHeader';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
-      <header style={{ marginBottom: '30px', borderBottom: '1px solid #eee', paddingBottom: '10px' }}>
-        <h1>MarketPlace</h1>
-      </header>
-      
-      <main>
-        <ProductList />
-      </main>
+    <div className={styles.mainContainer}>
+      <ProfileHeader />
+      <ProductList />
     </div>
   );
 }
